6259a8ec387c7987a7b8419d919a887fc8142ac9
SniffieFTW <brodycole929@gmail.com>
2020-08-18 12:43:50 -0500

    Update Test.java
    added more commands to the main class

--- src/main/java/me/sniffieftw/p1/test/Test.java
+++ src/main/java/me/sniffieftw/p1/test/Test.java
@@ -1,6 +1,7 @@
 package me.sniffieftw.p1.test;
 
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Test extends JavaPlugin {
@@ -18,9 +19,15 @@ public final class Test extends JavaPlugin {
 
     public void registerCommands (){
         getCommand("test").setExecutor(new commands());
+        getCommand("spam").setExecutor(new commands());
+        getCommand("craft").setExecutor(new commands());
+        getCommand("fly").setExecutor(new commands());
+        getCommand("heals").setExecutor(new commands());
+        getCommand("dog").setExecutor(new commands());
+        getCommand("kit").setExecutor(new commands());
+    }
+    public void registerEvents(){
+        PluginManager pm = getServer().getPluginManager();
 
     }
-
-
-
 }
